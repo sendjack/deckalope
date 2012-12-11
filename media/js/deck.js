ed26f0626296b6f3fcca1ffd4b0fc2a7ccef0023
Jon Warman <warman@sendjack.com>
2012-12-11 15:20:49 -0500

    [deck] update favicon, move preloading to our js file

--- media/js/deck.js
+++ media/js/deck.js
@@ -3,3 +3,19 @@ function preload(arrayOfImages) {
         $('<img/>')[0].src = this;
     });
 }
+
+preload([
+        'deck/Cover.jpg',
+        'deck/Team.jpg',
+        'deck/Problem1.jpg',
+        'deck/Problem2.jpg',
+        'deck/Problem3.jpg',
+        'deck/Market.jpg',
+        'deck/Trend.jpg',
+        'deck/GoToMarket1.jpg',
+        'deck/GoToMarket2.jpg',
+        'deck/AndMore1.jpg',
+        'deck/AndMore2.jpg',
+        'deck/Revenue.jpg',
+        'deck/Financials.jpg'
+]);
